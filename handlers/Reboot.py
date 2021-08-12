411050399dffc380930292bad80c0ef3c0ca7b07
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-08-12 13:47:37 +0530

    Create Reboot.py

---
+++ handlers/Reboot.py
@@ -0,0 +1,19 @@
+import os
+
+import sys
+
+from pyrogram import filters, Client
+
+from pyrogram.types import Message
+
+from config import SUDO_USERS
+
+@Client.on_message(filters.command("restart") & filters.user(SUDO_USERS))
+
+async def restart(c: Client, m: Message):
+
+    await m.reply_text("Rebooting Omen......")
+
+    args = [sys.executable, "-m", "main.py"]
+
+    os.execl(sys.executable, *args)
